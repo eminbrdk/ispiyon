591413339e36b18327ddc36ce9f088c1dd0d5fc6
emininmac <mamihaha4@gmail.com>
2023-03-13 13:16:31 +0300

    Initial Commit

---
+++ ispiyon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ispiyon
+//
+//  Created by Muhammed Emin Bardakcı on 13.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
